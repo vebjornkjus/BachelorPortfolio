b22a824dda87a69c728287a7674664d46299223a
Kristiank02 <Kristian.kalleberg@hotmail.no>
2025-09-05 15:46:52 +0200

    Oppdatering

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="glass-header text-white py-4 md:py-6">
       <div className="text-center text-gray-400 text-sm md:text-base">
-        <p>2025 Bachelorgruppe 11</p>
+        <p>2025 Bachelorgruppe 11.</p>
       </div>
     </footer>
   );
